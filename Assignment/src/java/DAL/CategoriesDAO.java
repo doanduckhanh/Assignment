73d64a872c26c457db0eef6c96e68dce35c3435e
kid211101 <khanhddhe150956@fpt.edu.vn>
2022-03-06 21:47:03 +0700

    Add Categories

--- Assignment/src/java/DAL/CategoriesDAO.java
+++ Assignment/src/java/DAL/CategoriesDAO.java
@@ -5,12 +5,41 @@
  */
 package DAL;
 
-import model.Categories;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Category;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Book;
 
 /**
  *
  * @author khanh doan
  */
-public class CategoriesDAO extends BaseDAO<Categories>{
-    
+public class CategoriesDAO extends BaseDAO<Category>{
+    public ArrayList<Category> getAll(){
+        ArrayList<Category> categories = new ArrayList<>();
+        try {
+            String sql = "SELECT [CateID]\n"+
+                    ", [CateName]\n"+
+                    "FROM [Categories]";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next())
+           {
+               Category ca = new Category();
+               ca.setID(rs.getInt("CateID"));
+               ca.setName(rs.getString("CateName"));
+               categories.add(ca);
+           }
+        } catch (SQLException ex) {
+            Logger.getLogger(CategoriesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return categories;
+    }
+    public ArrayList<Book> getBooksByCategories(String Cate){
+        
+    }
 }
