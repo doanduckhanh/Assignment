45c73e491d93c42b7cf10ec5bb4f2d3a139868bb
kid211101 <khanhddhe150956@fpt.edu.vn>
2022-03-06 10:41:50 +0700

    Add user.jsp

---
+++ Assignment/build/generated/src/org/apache/jsp/user_jsp.java
@@ -0,0 +1,128 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class user_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Library Management</title>\n");
+      out.write("        <!-- Favicon-->\n");
+      out.write("        <link rel=\"icon\" type=\"image/x-icon\" href=\"assets/favicon.ico\" />\n");
+      out.write("        <!-- Core theme CSS (includes Bootstrap)-->\n");
+      out.write("        <link href=\"css/styles.css\" rel=\"stylesheet\" />\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>\n");
+      out.write("        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <!-- Responsive navbar-->\n");
+      out.write("        <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n");
+      out.write("            <div class=\"container px-5\">\n");
+      out.write("                <a href=\"#\" class=\"navbar-brand has-logo\">\n");
+      out.write("                    <span class=\"logo d-inline\">\n");
+      out.write("                        <img src=\"https://cdn.discordapp.com/attachments/387267441245552642/942414648764944424/unknown.png\" height=\"40\" width=\"110\" alt=\"reading\">\n");
+      out.write("                    </span>\n");
+      out.write("                </a>\n");
+      out.write("                <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"><span class=\"navbar-toggler-icon\"></span></button>\n");
+      out.write("                <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n");
+      out.write("                    <ul class=\"navbar-nav ms-auto mb-2 mb-lg-0\">\n");
+      out.write("                        <li class=\"nav-item\"><a class=\"nav-link active\" aria-current=\"page\" href=\"index.jsp\">Home</a></li>                       \n");
+      out.write("                        <li class=\"nav-item\"><a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Name</a></li>\n");
+      out.write("                    </ul>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("        </nav>\n");
+      out.write("        <!-- Search Box -->\n");
+      out.write("        <div id=\"search-box\" class=\"s130\">\n");
+      out.write("            <form>\n");
+      out.write("                <div class=\"inner-form\">\n");
+      out.write("                    <div class=\"input-field first-wrap\">\n");
+      out.write("                        <div class=\"svg-wrapper\">\n");
+      out.write("                            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\">\n");
+      out.write("                                <path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z\"></path>\n");
+      out.write("                            </svg>\n");
+      out.write("                        </div>\n");
+      out.write("                        <input name=\"search\" type=\"text\" value=\"\">\n");
+      out.write("                    </div>\n");
+      out.write("                    <div class=\"input-field second-wrap\">\n");
+      out.write("                        <button class=\"btn-search\" type=\"submit\">SEARCH</button>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("            </form>\n");
+      out.write("        </div>\n");
+      out.write("        <div class=\"row g-0\">\n");
+      out.write("            <div class=\"col-6 col-md-2\" id=\"categories\">\n");
+      out.write("                <h3>Categories</h3>\n");
+      out.write("                <ul class=\"list-group list-group-flush\">\n");
+      out.write("                    <li class=\"list-group-item\">An item</li>\n");
+      out.write("                    <li class=\"list-group-item\">A second item</li>\n");
+      out.write("                    <li class=\"list-group-item\">A third item</li>\n");
+      out.write("                    <li class=\"list-group-item\">A fourth item</li>\n");
+      out.write("                    <li class=\"list-group-item\">And a fifth one</li>\n");
+      out.write("                </ul>               \n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"col-sm-6 col-md-10\">\n");
+      out.write("                <table>\n");
+      out.write("                    \n");
+      out.write("                </table>\n");
+      out.write("            </div>          \n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
