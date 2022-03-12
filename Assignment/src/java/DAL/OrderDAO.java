829607a166816662b1ff9024d9ee2b8353e5ad29
kid211101 <khanhddhe150956@fpt.edu.vn>
2022-03-12 10:11:21 +0700

    Update OrderDAO

--- Assignment/src/java/DAL/OrderDAO.java
+++ Assignment/src/java/DAL/OrderDAO.java
@@ -49,4 +49,28 @@ public class OrderDAO extends BaseDAO<Order>{
         }
         return arr;
     }
+    
+    public ArrayList<Order> getByCus(String cusid){
+        ArrayList<Order> list = new ArrayList<>();
+        String sql = "SELECT * FROM [Order] WHERE [CusID] = ?";
+        try {
+           PreparedStatement statement = connection.prepareStatement(sql);
+           ResultSet rs = statement.executeQuery();
+           statement.setString(1, cusid);
+           while(rs.next())
+           {
+               Order or = new Order();
+               or.setOrid(rs.getInt(1));
+               or.setCus(rs.getString(2));
+               or.setBook(rs.getString(3));
+               or.setStart(rs.getDate(4));
+               or.setEnd(rs.getDate(5));       
+               or.setStatus(rs.getBoolean(6));
+               list.add(or);
+           }
+        } catch (Exception e) {
+                Logger.getLogger(OrderDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return list;
+    }
 }
